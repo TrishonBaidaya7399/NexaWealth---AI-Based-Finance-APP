8cfdfaf6bad0bb2cc87830efdcf412dbe1fdebdd
trishonbaidaya7399 <githubtrishon@gmail.com>
2025-02-14 23:42:27 +0600

    layout changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/shared/Footer/Footer";
+import TopHeader from "@/components/shared/Header/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className}`}>
         {/* -------------- header -------------- */}
+        <TopHeader />
         <main className="min-h-screen">{children}</main>
         {/* -------------- footer -------------- */}
         <Footer />
